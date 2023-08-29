986489122cd090fe419e483b9955bf0135e15dcd
kwonyongil <kkwonyi95@gmail.com>
2023-08-29 11:24:43 +0900

    viewtype finish

--- app/src/main/java/com/example/contacts/adapter.kt
+++ app/src/main/java/com/example/contacts/adapter.kt
@@ -4,28 +4,62 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.contacts.databinding.ItemBinding
+import com.example.contacts.databinding.ItemFavoriteBinding
 
-class Adapter(val mItems: MutableList<MyItem>) : RecyclerView.Adapter<Adapter.Holder>() {
+class Adapter(val mItems: MutableList<MyItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    companion object {
+        const val BasicView = 0
+        const val FavoriteView = 1
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            BasicView -> {
+                val binding = ItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                BasicHolder(binding)
+            }
+            FavoriteView -> {
+                val binding = ItemFavoriteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                FavoriteHolder(binding)
+            }
+            else -> throw IllegalArgumentException("Error")
+        }
+    }
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = mItems[position]
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        val binding = ItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return Holder(binding)
+        when (holder) {
+            is BasicHolder -> {
+                holder.binding.itemImg.setImageResource(item.img)
+                holder.binding.itemName.text = item.name
+                holder.binding.itemPhoneNumber.text = item.number
+                holder.binding.favoriteBtn.setOnClickListener {
+                    item.isFavorite = !item.isFavorite
+                    notifyItemChanged(position)
+                }
+            }
+            is FavoriteHolder -> {
+                holder.binding.itemFavoriteImg.setImageResource(item.img)
+                holder.binding.itemFavoriteName.text = item.name
+                holder.binding.itemFavoritePhoneNumber.text = item.number
+                holder.binding.favoriteBtnFill.setOnClickListener {
+                    item.isFavorite = false
+                    notifyItemChanged(position)
+                }
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.binding.itemImg.setImageResource(mItems[position].img)
-        holder.binding.itemName.text = (mItems[position].name)
-        holder.binding.itemPhoneNumber.text = (mItems[position].number)
+    override fun getItemViewType(position: Int): Int {
+        return if (mItems[position].isFavorite) {
+            FavoriteView
+        } else {
+            BasicView
+        }
     }
-
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
+    inner class BasicHolder(val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class FavoriteHolder(val binding: ItemFavoriteBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun getItemCount(): Int {
         return mItems.size
-    }
-
-    inner class Holder(val binding: ItemBinding) : RecyclerView.ViewHolder(binding.root){
     }
 }
